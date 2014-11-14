da5446824f12857ad4e0ea070a940f4eeb5ea63d
Lew Ayotte <lew@ithemes.com>
2014-11-14 15:28:03 -0600

    Tagging 0.0.2

--- exchange-addon-table-rate-shipping.php
+++ exchange-addon-table-rate-shipping.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Table Rate Shipping
- * Version: 0.0.1 
+ * Version: 0.0.2
  * Description: Adds Table Rate Shipping to iThemes Exchange.
  * Plugin URI: http://ithemes.com/exchange/table-rate-shipping/
  * Author: iThemes
